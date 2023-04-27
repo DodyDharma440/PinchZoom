90b7d9939d76a1e5e6731ee5ea705cfe42cc91b9
DodyDharma440 <dharma.dody@yahoo.com>
2023-04-27 20:41:36 +0800

    Initial Commit

---
+++ PinchZoom/PinchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomApp.swift
+//  PinchZoom
+//
+//  Created by Dodi Aditya on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
