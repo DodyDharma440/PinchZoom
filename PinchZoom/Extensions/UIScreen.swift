eb2cd8b7a2b34d004e08e55de6c7f9d5a325acb3
DodyDharma440 <dharma.dody@yahoo.com>
2023-04-28 22:55:51 +0800

    info panel

---
+++ PinchZoom/Extensions/UIScreen.swift
@@ -0,0 +1,15 @@
+//
+//  UIScreen.swift
+//  PinchZoom
+//
+//  Created by Dodi Aditya on 28/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+    static let screenSize = UIScreen.main.bounds.size
+}
--- PinchZoom/Screens/ContentView.swift
+++ PinchZoom/Screens/ContentView.swift
@@ -12,14 +12,24 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
+                
                 // MARK: - IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -28,6 +38,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
                 // MARK: - DOUBLE TAP GESTURE
@@ -37,19 +48,37 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                // MARK: - DRAG GESTURE
+                    .gesture(DragGesture()
+                        .onChanged { gesture in
+                            withAnimation(.linear(duration: 1)) {
+                                imageOffset = gesture.translation
+                            }
+                        }
+                        .onEnded { _ in
+                            if imageScale == 1 {
+                                resetImageState()
+                            }
+                        }
+                    ) // Gesture
             } // ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear(perform: {
                 isAnimating = true
             })
-        } //: Navigation
-    }
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
+        }
+    } // Navigation
 }
 
 // MARK: - PREVIEW
