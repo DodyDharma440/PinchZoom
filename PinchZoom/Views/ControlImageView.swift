9bfd2c607dc600233b714764cb8da5cf448ff3d5
DodyDharma440 <dharma.dody@yahoo.com>
2023-04-28 23:14:14 +0800

    control scale interface

---
+++ PinchZoom/Views/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  PinchZoom
+//
+//  Created by Dodi Aditya on 28/04/23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
